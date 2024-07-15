e001a08517c188f3cb4d564050d92352423ee947
WeiHongWi <willy@Andrew>
2024-07-16 05:03:58 +0800

    Implement the function for table Transac's CRUD.

--- sql_go/transac.sql.go
+++ sql_go/transac.sql.go
@@ -1,1 +1,109 @@
 package CRUD
+
+import (
+	"context"
+)
+
+const CreateTransacSQL = `
+INSERT INTO Transac
+("From_account_id","To_account_id","Amount")
+VALUES
+($1,$2,$3)
+RETURNING "ID","From_account_id","To_account_id","Amount","Createdat"
+`
+
+type CreateTransacParam struct {
+	From_account_id int64
+	To_account_id   int64
+	Amount          int64
+}
+
+func (q *Queries) CreateTransac(ctx context.Context, arg CreateTransacParam) (Transac, error) {
+	row := q.db.QueryRowContext(ctx, CreateTransacSQL, arg.From_account_id, arg.To_account_id, arg.Amount)
+
+	var T Transac
+	err := row.Scan(
+		&T.ID,
+		&T.FromAccountID,
+		&T.ToAccountID,
+		&T.Amount,
+		&T.Createdat,
+	)
+	return T, err
+}
+
+const GetTransacSQL = `
+SELECT "ID","From_account_id","To_account_id","Amount","Createdat"
+FROM "Transac"
+WHERE "ID" = $1
+LIMIT 1
+`
+
+type GetTransacParam struct {
+	ID int64
+}
+
+func (q *Queries) GetTransac(ctx context.Context, arg GetAccountParam) (Transac, error) {
+	row := q.db.QueryRowContext(ctx, GetTransacSQL, arg.ID)
+
+	var T Transac
+	err := row.Scan(
+		&T.ID,
+		&T.FromAccountID,
+		&T.ToAccountID,
+		&T.Amount,
+		&T.Createdat,
+	)
+	return T, err
+}
+
+const ListTransacSQL = `
+SELECT "ID","From_account_id","To_account_id","Amount","Createdat"
+FROM "Transac"
+ORDER BY "ID"
+WHERE "From_account_id" = $1 OR
+      "To_account_id" = $2
+LIMIT $3
+OFFSET $4
+`
+
+type ListTransacParam struct {
+	From_account_id int64
+	To_account_id   int64
+	Limit           int32
+	Offset          int32
+}
+
+func (q *Queries) ListTransac(ctx context.Context, arg ListTransacParam) ([]Transac, error) {
+	rows, err := q.db.QueryContext(ctx, ListTransacSQL, arg.From_account_id, arg.To_account_id, arg.Limit, arg.Offset)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var T_arr []Transac
+
+	for rows.Next() {
+		var T Transac
+		if err := rows.Scan(
+			&T.ID,
+			&T.FromAccountID,
+			&T.ToAccountID,
+			&T.Amount,
+			&T.Createdat,
+		); err != nil {
+			return nil, err
+		}
+		T_arr = append(T_arr, T)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return T_arr, nil
+}
